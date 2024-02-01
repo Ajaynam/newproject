260936af95d4a77b4857a727655834351d3b3138
Ajaynam <ajaynamdev701@gmail.com>
2024-02-01 18:29:04 +0530

    Add new

--- db/db.js
+++ db/db.js
@@ -21,11 +21,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'newdata',
-  port: process.env.DB_PORT || 3306,
 });
 
 db.connect((err) => {
